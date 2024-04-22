8fead25e82e72d823b21c320afb011bd0fcde35f
yasemintk3 <yasemin.tok0702@gmail.com>
2024-04-22 16:46:17 +0300

    theMovieApp

---
+++ TheMovieApp/TheMovieApp/MoviePage.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovieApp
+//
+//  Created by Yasemin TOK on 22.04.2024.
+//
+
+import UIKit
+
+class MoviePage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
